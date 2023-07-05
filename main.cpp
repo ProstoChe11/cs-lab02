4a75b00e7e588abed84d6c73c58b5af5917eb6af
Bob (TrofimovIE) <bob@example.com>
2023-07-05 16:11:50 +0300

    code: добавлена печать минимума

--- main.cpp
+++ main.cpp
@@ -9,9 +9,15 @@ int main()
 	cin >> a >> b;
 
 	cout << "A + B = " << a + b << '\n'
-		 << "A - B = " << a - b << '\n'
-         << "A * B = " << a * b << '\n'
-		 << "A / B = " << a / b << '\n';
+	     << "A - B = " << a - b << '\n'
+             << "A * B = " << a * b << '\n'
+	     << "A / B = " << a / b << '\n';
 
+        if (a < b){
+		cout << a;
+	}
+	else{
+		cout << b;
+	}
     return 0;
 }
